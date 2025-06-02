fb3d2640ecf3d0a08431cb02318f76d52130e401
WillDSadler <willsadler@outlook.com>
2025-06-01 21:39:17 -0400

    Create SortByName.java

---
+++ studentdata/SortByName.java
@@ -0,0 +1,9 @@
+package studentdata;
+
+import java.util.Comparator;
+
+public class SortByName implements Comparator<Student> {
+    public int compare(Student a, Student b) {
+        return a.name.compareToIgnoreCase(b.name);
+    }
+}
