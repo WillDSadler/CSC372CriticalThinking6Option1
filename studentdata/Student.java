fe8743362e3ae3d4b38ca241e65ab44a52a059d7
WillDSadler <willsadler@outlook.com>
2025-06-01 21:38:38 -0400

    Create Student.java

---
+++ studentdata/Student.java
@@ -0,0 +1,18 @@
+package studentdata;
+
+public class Student {
+    int rollno;
+    String name;
+    String address;
+
+    public Student(int rollno, String name, String address) {
+        this.rollno = rollno;
+        this.name = name;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return rollno + " " + name + " " + address;
+    }
+}
